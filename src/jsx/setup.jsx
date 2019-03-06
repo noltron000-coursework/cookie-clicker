09aa7c8d4051f43f6123479f3582f452320a2679
Nolan Kovacik <noltron000@gmail.com>
2019-03-06 15:48:13 -0800

    🙌 updating most quotation marks to single-quotes

--- src/jsx/setup.jsx
+++ src/jsx/setup.jsx
@@ -8,16 +8,16 @@ import logo from '../img/logo.svg';
 // use es6 function for non-dynamic assets
 const Setup = () => {
 	return (
-		<header className="app-header">
-			<img src={logo} className="app-logo" alt="logo" />
+		<header className='app-header'>
+			<img src={logo} className='app-logo' alt='logo' />
 			<p>
 				Edit <code>src/App.js</code> and save to reload.
 					</p>
 			<a
-				className="app-link"
-				href="https://reactjs.org"
-				target="_blank"
-				rel="noopener noreferrer"
+				className='app-link'
+				href='https://reactjs.org'
+				target='_blank'
+				rel='noopener noreferrer'
 			>
 				Learn React
 					</a>
